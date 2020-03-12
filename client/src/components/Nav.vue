620087523451e20986c10bd636a93028ff2d4b38
sanjay reddy m <msanjayreddy007@gmail.com>
2020-03-12 13:18:43 -0400

    commit

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,37 +1,43 @@
 <template>
-<nav class="navbar container" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="/">
-      <strong class="is-size-4">Regular Fitness </strong>
-    </a>
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    
+
+    <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-  <div id="navbar" class="navbar-menu">
+
+  <div :class="{ 'is-active':isOpen }" class="navbar-menu">
     <div class="navbar-start">
-      <router-link to="/" class="navbar-item">Home</router-link>
-      <router-link to="/about" class="navbar-item">About</router-link>
+      <router-link to="/" class="navbar-item" active-class="is-current">Home</router-link> 
+      <router-link to="/about" class="navbar-item" active-class="is-current">About</router-link>
+      
     </div>
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a class="button is-dark">
-            <strong>Sign In</strong>
+          <a class="button is-light" href="/SigIn">
+         SignIn
           </a>
         </div>
       </div>
     </div>
   </div>
+  
 </nav>
 </template>
+
 <script>
 export default {
-    name: 'Nav'
+data: ()=>({
+  isOpen: false 
+ })
 }
 </script>
+
 <style lang="scss" scoped>
   nav {
     margin-top: 25px;
@@ -40,7 +46,7 @@ export default {
       font-weight: bold;
       color: #2c3e50;
       &.router-link-exact-active {
-        color: #d88d00;
+        color:  #d45757;
       }
     }
   }
--- client/src/components/SignIn.vue
+++ client/src/components/SignIn.vue
@@ -1,38 +1,65 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/SignIn.vue'
+<template>
+<form class="container" @submit="SignIn">
+  {{error}}
+    <div class="field">
+  <p class="control has-icons-left has-icons-right">
+    <input class="input" type="email" placeholder="Email">
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control has-icons-left">
+    <input class="input" type="password" placeholder="Password">
+    <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control">
+    
+    <button class="button is-dark">
+      SignIn
+    </button>
+  </p>
+</div>
+</form>
+</template>
 
-Vue.use(VueRouter)
+<script>
+    export default {
+        name: 'SignIn',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
+</script>
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-    {
-    path: '/SignIn',
-    name: 'SignIn',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: SignIn
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+<style >
+  
+    
+</style>
